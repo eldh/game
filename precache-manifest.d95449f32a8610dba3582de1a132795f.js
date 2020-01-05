db3fec77648f4ea3b634a8391bffccdb87070181
eldh <andreas.eldh@gmail.com>
2020-01-05 13:20:10 +0100

    Updates

---
+++ precache-manifest.d95449f32a8610dba3582de1a132795f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2bdae7f5cd1f3fed3e9fab124b7cd5b",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "22f149e044ba48027c3d",
+    "url": "/game/static/css/2.18ba028f.chunk.css"
+  },
+  {
+    "revision": "5aabfaa5bdc783854d9b",
+    "url": "/game/static/css/main.dcb68c92.chunk.css"
+  },
+  {
+    "revision": "22f149e044ba48027c3d",
+    "url": "/game/static/js/2.b646879d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/game/static/js/2.b646879d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5aabfaa5bdc783854d9b",
+    "url": "/game/static/js/main.25ceff8e.chunk.js"
+  },
+  {
+    "revision": "ee117b7ce9ba93afaa11",
+    "url": "/game/static/js/runtime-main.a6f4b19e.js"
+  }
+]);
